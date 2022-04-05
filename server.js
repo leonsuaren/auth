46b68ebb6e57e5b70cdc2bd1f11ea812784303c3
Leon Suarez <leonsua@gmail.com>
2022-04-05 12:27:57 -0400

    added the protect middleware in the routes now we need a jwt to acces to the roures and login before accessing to the routes

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
 const app = express();
-const routes = require('./routes/auth');
+const auth = require('./routes/auth');
+const private = require('./routes/private');
 connectDB();
 
 app.use(express.json());
 
-app.use('/api/auth', routes);
+app.use('/api/auth', auth);
+app.use('/api/private', private);
 
 //Error handler
 app.use(errorHandler);
