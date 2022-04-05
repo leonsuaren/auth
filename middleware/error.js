c9768711153e07cf25419dcea81f7de9db93bc63
Leon Suarez <leonsua@gmail.com>
2022-04-05 11:00:34 -0400

    added the error handler to the register and the login route

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,9 @@
 const ErrorResponse = require('../utils/errorResponse');
 
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   let error = { ...err };
 
   error.message = err.message;
-
-  console.log(err);
 
   if(err.code ===11000) {
     const message = 'Duplicate Faild Value Enter';
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: "./config.env"});
 const express = require('express');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 const app = express();
 const routes = require('./routes/auth');
@@ -9,6 +10,9 @@ connectDB();
 app.use(express.json());
 
 app.use('/api/auth', routes);
+
+//Error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
