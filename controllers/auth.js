de7ebade5255914b897d2acea7939a97ef03599f
Leon Suarez <leonsua@gmail.com>
2022-04-05 14:40:00 -0400

    handle the forgotpassword route and set up the sendEmail function

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,8 +58,33 @@ exports.login = async (req, res, next) => {
 
 }
 
-exports.forgotpassword = (req, res, next) => {
-  res.send("Forgot Password route");
+exports.forgotpassword = async (req, res, next) => {
+  const {email} = req.body;
+
+  try {
+
+    const user = await User.findOne((email));
+    if(!user) {
+      return next(new ErrorResponse("Email could not be sent", 404));
+    }
+
+    const resetToken = user.getResetPasswordToken()
+
+    await user.save();
+    const resetUrl = `http://localhost:3001/passwordreset/${resetToen}`;
+    const message = `
+      <h1>You have requested a password reset</h1>
+      <p>Please go to this link to reset your password</p>
+      <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+    `;
+    try {
+
+    } catch (error) {
+
+    }
+  } catch (error) {
+  }
+
 }
 
 exports.resetpassword = (req, res, next) => {
