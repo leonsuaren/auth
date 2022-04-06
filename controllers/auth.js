40827398237b72a285d7c4535473d7e204530d2f
Leon Suarez <leonsua@gmail.com>
2022-04-06 19:28:23 -0400

    some changes on the way I sent the response to the client

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,7 +77,7 @@ exports.forgotpassword = async (req, res, next) => {
     await user.save();
 
     // Create reset url to email to provided email
-    const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
+    const resetUrl = `http://localhost:3001/passwordreset/${resetToken}`;
 
     // HTML Message
     const message = `
